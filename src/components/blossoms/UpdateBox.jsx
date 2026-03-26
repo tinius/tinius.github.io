951828b8f513634e5ed9cc7604e8239612d09917
Niko Kommenda <nikokommenda@Nikos-MacBook-Pro.local>
2026-03-26 16:11:48 +0000

    add box with peak bloom declaration

---
+++ src/components/blossoms/UpdateBox.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const UpdateBox = () => {
+
+  return <div
+  className='update-div'
+  >
+    <h2>March 26 update: NPS declares peak bloom!</h2>
+
+    <p>My model’s final prediction, which had been stable for a few days, was only two days off. A more detailed analysis of the model’s performance will follow.
+</p><p>
+You can see the model’s final output as of the morning of March 26 below.
+</p>
+  </div>
+
+}
+
+export default UpdateBox
